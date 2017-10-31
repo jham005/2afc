c57b1faa45c2f5eb99f273290d69dbc843cbc7e3
John Hamer <hamer@dcs.gla.ac.uk>
2017-10-31 20:29:14 +0000

    Provide Upload button. Various bug fixes.

--- rename.php
+++ rename.php
@@ -1,2 +1,17 @@
 <?php
-  // rename prev to curr
+foreach (array('e', 'prev', 'curr') as $f)
+  if (!is_string($_REQUEST[$f]) || $_REQUEST[$f] == '.' || $_REQUEST[$f] == '..') {
+    header('HTTP/1.0 400 Bad Request');
+    exit();    
+  }
+
+require 'util.php';
+
+$e = filename_safe($_REQUEST['e']);
+$prev = filename_safe($_REQUEST['prev']);
+$curr = filename_safe($_REQUEST['curr']);
+
+if (!rename("experiments/$e/$prev", "experiments/$e/$curr"))
+  header('HTTP/1.0 404 Not Found');
+else
+  header('HTTP/1.0 204 No Content');
--- upload.js
+++ upload.js
@@ -1,46 +1,71 @@
+var dropDir = "";
 var r = new Resumable({
     target: "upload.php",
+    query: function() {
+	return {e: $('#experiment').val(), dir: dropDir};
+    },
     testChunks: true
 });
- 
-r.assignBrowse(document.getElementById("add-file-btn"), true);
-r.assignDrop(document.getElementById('drop-target'));
-$('#drop-target')
-    .on('dragenter', function() { $(this).addClass('resumable-dragover'); })
-    .on('dragend', function() { $(this).removeClass('resumable-dragover'); })
-    .on('drop', function() { $(this).removeClass('resumable-dragover'); });
 
-$("#start-upload-btn").click(function() {
-    r.upload();
+$(document).on('focusin', 'input', function(){
+    $(this).data('val', $(this).val());
 });
- 
-$("#pause-upload-btn").click(function() {
-    if (r.files.length > 0) {
-        if (r.isUploading())
-            return r.pause();
-        return r.upload();
-    }
+
+$('#experiment').change(function() {
+    $.get('load-experiment.php', {e: $(this).val()}, null, "json")
+	.done(function(data) {
+	    $('#drop-target').empty();
+	    $.map(data, function(files, folderName) {
+		var div = $("<div>");
+		var input = $("<input type='text' value='" + folderName + "'/>")
+		    .change(function() {
+			$.post("rename.php", {e: $('#experiment').val(), prev: $(this).data('val'), curr: $(this).val()});
+		    });
+		if (folderName == "") input.hide();
+		div.append(input);
+		var ul = $("<ul>");
+		$.map(files, function(file) {
+		    ul.append($("<li>").text(file));
+		});
+		ul.sortable({
+		    connectWith: "ul",
+		    receive: function(event, ui) {
+			var item = $(ui.item[0]);
+			var srcFolder = $(ui.sender[0]).parent('div').find('input').val();
+			var dstFolder = item.parent().parent().find('input').val();
+			$.post("move.php", {e: $('#experiment').val(), item: item.text(), srcFolder: srcFolder, dstFolder: dstFolder});
+		    }
+		});
+		div.append(ul);
+		$('#drop-target').append(div);
+	    });
+	    r.assignDrop($('#drop-target ul'));
+	    $('#drop-target ul')
+		.on('dragenter', function() { $(this).addClass('resumable-dragover'); })
+		.on('dragend', function() { $(this).removeClass('resumable-dragover'); })
+		.on('drop', function(event) {
+		    $(this).removeClass('resumable-dragover')
+		    dropDir = $(event.target).parent().parent().find('input').val();
+		});
+	});
 });
+
+$('#upload-btn').click(function() { r.upload(); });
  
-var progressBar = new ProgressBar($("#upload-progress"));
+var progressBar = new ProgressBar("#upload-progress");
  
 r.on("fileAdded", function(file, event) {
-    $('#drop-target ul').append($('<li>').text(file.fileName));
+    $(event.target).parent().append($('<li>').text(file.fileName).css("opacity", "0.3"));
     progressBar.fileAdded();
 });
 
 r.on("fileSuccess", function(file, message) {
-    $('#drop-target ul li').filter(function() { return $(this).text() == file.fileName; }).remove();
+    $('#drop-target li').filter(function() { return $(this).text() == file.fileName; }).css("opacity", "1");
     progressBar.finish();
 });
  
 r.on("progress", function() {
     progressBar.uploading(r.progress() * 100);
-    $("#pause-upload-btn").find(".glyphicon").removeClass("glyphicon-play").addClass("glyphicon-pause");
-});
-
-r.on("pause", function() {
-    $("#pause-upload-btn").find(".glyphicon").removeClass("glyphicon-pause").addClass("glyphicon-play");
 });
 
 function ProgressBar(ele) {
@@ -51,7 +76,7 @@ function ProgressBar(ele) {
     },
  
     this.uploading = function(progress) {
-        (this.thisEle).find(".progress-bar").attr("style", "width:"+progress+"%");
+        (this.thisEle).find(".progress-bar").attr("style", "width:" + progress + "%");
     },
     
     this.finish = function() {
