0595cefda858b516bf8b412c73076f769cb10305
John Hamer <John.Hamer@glasgow.ac.uk>
2017-11-14 22:17:31 +0000

    In load-experiment.php, check for invalidDir and respond with 400

--- load-experiment.php
+++ load-experiment.php
@@ -1,7 +1,12 @@
 <?php
 require 'util.php';
 $e = filename_safe(trim($_GET['e']));
-if (empty($e) || !is_dir("experiments/$e")) {
+if (empty($e) || invalidDir($e)) {
+  header('HTTP/1.0 400 Bad Request');
+  exit();
+}
+
+if (!is_dir("experiments/$e")) {
   header('HTTP/1.0 404 Not Found');
   exit();
 }
